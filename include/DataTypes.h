a866807ee3e20cd913631285a827d3ded476b298
Sooraj <soomodi@gmail.com>
2020-11-16 05:11:30 +0000

    Add DataTypes file

---
+++ include/DataTypes.h
@@ -0,0 +1,22 @@
+#ifndef _DATA_TYPES_
+#define _DATA_TYPES_
+
+typedef struct person {
+  char* firstName;
+  char* lastName;
+  int age;
+} Person;
+
+void printPerson(void* toBePrinted);
+int comparePerson(const void* first, const void* second);
+void deletePerson(void* toBeDeleted);
+
+void printString(void* toBePrinted);
+int compareString(const void* first, const void* second);
+void deleteString(void* toBeDeleted);
+
+void printInt(void* toBePrinted);
+int compareInt(const void* first, const void* second);
+void deleteInt(void* toBeDeleted);
+
+#endif
---
+++ src/DataTypes.c
@@ -0,0 +1,51 @@
+#include "DataTypes.h"
+
+// Person Functions
+
+void printPerson(void* toBePrinted) {
+  Person* toPrint = (Person *)toBePrinted;
+  printf("{
+    firstName: %s,
+    lastName: %s,
+    age: %d
+  }\n", toPrint->firstName, toPrint->lastName, toPrint->age);
+}
+
+int comparePerson(const void* first, const void* second) {
+  return (((Person *)first)->age - ((Person *)second)->age);
+}
+
+void deletePerson(void* toBeDeleted) {
+  Person* toDelete = (Person *)toBeDeleted;
+  free(toDelete->firstName);
+  free(toDelete->lastName);
+  free(toDelete);
+}
+
+// String Functions
+
+void printString(void* toBePrinted) {
+  printf("%s\n", (char *)toBePrinted);
+}
+
+int compareString(const void* first, const void* second) {
+  return strcmp((char *)first, (char *)second);
+}
+
+void deleteString(void* toBeDeleted) {
+  free((char *)toBeDeleted);
+}
+
+// Integer Functions
+
+void printInt(void* toBePrinted) {
+  printf("%d\n", (int *)toBePrinted);
+}
+
+int compareInt(const void* first, const void* second) {
+  return (((int *)first) - ((int *)second));
+}
+
+void deleteInt(void* toBeDeleted) {
+  free((int *)toBeDeleted);
+}
