d0ac05b207a68590ca563280a065f5ed3b7d9641
SoorajModi <soomodi@gmail.com>
2021-02-24 21:22:28 -0500

    Add tests for stack

--- test/StackAPITest.c
+++ test/StackAPITest.c
@@ -19,13 +19,83 @@ void should_initialize_stack(void) {
     TEST_ASSERT_NULL(stack->tail);
     TEST_ASSERT_EQUAL(0, stack->length);
 
-    deleteList(stack);
+    deleteStack(stack);
+}
+
+void should_push_data_onto_stack(void) {
+    Stack* stack = initializeStack(&printString, &deleteString, &compareString);
+
+    char *string = malloc(sizeof(char) * 6);
+    strncpy(string, "string", 6);
+    push(stack, string);
+
+    TEST_ASSERT_EQUAL(1, getSize(stack));
+    TEST_ASSERT_EQUAL(string, stack->head->data);
+
+    deleteStack(stack);
+}
+
+void should_get_data_from_top_of_stack(void) {
+    Stack* stack = initializeStack(&printString, &deleteString, &compareString);
+
+    char *string = malloc(sizeof(char) * 6);
+    strncpy(string, "string", 6);
+    push(stack, string);
+
+    TEST_ASSERT_EQUAL(string, top(stack));
+
+    deleteStack(stack);
+}
+
+void should_pop_data_from_top_of_stack(void) {
+    Stack* stack = initializeStack(&printString, &deleteString, &compareString);
+
+    char *string = malloc(sizeof(char) * 6);
+    strncpy(string, "string", 6);
+    push(stack, string);
+
+    TEST_ASSERT_EQUAL(string, top(stack));
+    TEST_ASSERT_EQUAL(1, getSize(stack));
+    TEST_ASSERT_FALSE(isStackEmpty(stack));
+
+    pop(stack);
+
+    TEST_ASSERT_EQUAL(0, getSize(stack));
+    TEST_ASSERT_TRUE(isStackEmpty(stack));
+
+    deleteStack(stack);
+}
+
+void should_clear_all_data_from__stack(void) {
+    Stack* stack = initializeStack(&printString, &deleteString, &compareString);
+
+    char *string = malloc(sizeof(char) * 6);
+    char *string2 = malloc(sizeof(char) * 7);
+    strncpy(string, "string", 6);
+    strncpy(string2, "string2", 7);
+
+    push(stack, string);
+    push(stack, string2);
+
+    TEST_ASSERT_EQUAL(2, getSize(stack));
+    TEST_ASSERT_FALSE(isStackEmpty(stack));
+
+    clearStack(stack);
+
+    TEST_ASSERT_EQUAL(0, getSize(stack));
+    TEST_ASSERT_TRUE(isStackEmpty(stack));
+
+    deleteStack(stack);
 }
 
 int main(void) {
     UNITY_BEGIN();
 
     RUN_TEST(should_initialize_stack);
+    RUN_TEST(should_push_data_onto_stack);
+    RUN_TEST(should_get_data_from_top_of_stack);
+    RUN_TEST(should_pop_data_from_top_of_stack);
+    RUN_TEST(should_clear_all_data_from__stack);
 
     return UNITY_END();
 }
